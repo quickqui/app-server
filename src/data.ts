a661c65995641e2ea9017199d4a2b8c665977916
nielinjie <nielinjie@gmail.com>
2019-08-29 21:13:22 +0800

    fake

--- src/data.ts
+++ src/data.ts
@@ -5,23 +5,23 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import fetch from 'node-fetch';
 import { createHttpLink } from 'apollo-link-http';
-import { DataProvider, forResource, DataProviderParams, chain } from '@quick-qui/data-provider';
+import { DataProvider, forResource, DataProviderParams, chain, fake } from '@quick-qui/data-provider';
 
 
 const link = createHttpLink({ uri: env.dataUrl, fetch: fetch });
 
-const fake: DataProvider =
-    async (fetchType: string, resource: string, params: DataProviderParams) => {
-        return {
-            data: [
-                { id: 1, name: "json" }
-            ]
-        }
-    }
+// const fake: DataProvider =
+//     async (fetchType: string, resource: string, params: DataProviderParams) => {
+//         return {
+//             data: [
+//                 { id: 1, name: "json" }
+//             ]
+//         }
+//     }
 
+const fakeP = fake({'User':[ { id: 1, name: "json" }]})
 
-
-const usersProvider = forResource("User", fake)
+const usersProvider = forResource("User", fakeP)
 
 const prismaProvider: Promise<DataProvider> = buildOpenCrudProvider({
     clientOptions: {
