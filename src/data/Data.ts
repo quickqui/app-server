b9fdec6370114c279aeb03ec5a8d745d010e6008
nielinjie <nielinjie@gmail.com>
2020-04-15 15:41:35 +0800

    相对路径require

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -5,19 +5,21 @@ import {
   dp,
   forResource,
   restDp,
-  withResourceMap
+  withResourceMap,
 } from "@quick-qui/data-provider";
 import {
   Exchange,
   parseRef,
   REF_RESOLVE,
-  withExchangeModel
+  withExchangeModel,
 } from "@quick-qui/model-defines";
 import assert from "assert";
 import _ from "lodash";
 import { model } from "../Model";
 import { resolve } from "../Resolve";
 import { fakeDataDataProvider } from "./FakeData";
+import { env } from "../Env";
+import p from "path";
 
 const backEndDataProvider: DataProvider = (
   type: string,
@@ -25,12 +27,14 @@ const backEndDataProvider: DataProvider = (
   params: DataProviderParams<unknown>
 ) => {
   //TODO 连上后面的dp，比如database的。
-  throw new Error(`no data provider find - ${JSON.stringify({ type, resource, ...params })}`);
+  throw new Error(
+    `no data provider find - ${JSON.stringify({ type, resource, ...params })}`
+  );
 };
 const thisEndDataProvider: Promise<DataProvider | undefined> = (async () => {
   const exchangeModel = withExchangeModel(await model)?.exchangeModel;
   const exchanges =
-    exchangeModel?.exchanges?.filter(exchange => {
+    exchangeModel?.exchanges?.filter((exchange) => {
       return exchange.to === "back" && exchange.from !== "fake";
     }) ?? [];
   if (_.isEmpty(exchanges)) return undefined;
@@ -45,21 +49,20 @@ const thisEndDataProvider: Promise<DataProvider | undefined> = (async () => {
         REF_RESOLVE,
         `only support resolve, but got "${protocol}"`
       );
-      const base =
-        exchange.annotations?.["buildingContext"]?.modelFile?.repositoryBase;
+      const modelFile = exchange.annotations?.["buildingContext"]?.modelFile;
+      const base = modelFile
+        ? p.resolve(env.extendPath, modelFile.relativeToModelDir)
+        : env.extendPath;
       const dataProvider = await resolve<DataProvider>(path, base);
       return forResource(exchange.resources, dataProvider);
     }
   });
-  return Promise.all(providers).then(dataPS => dataPS.reduce(chain));
+  return Promise.all(providers).then((dataPS) => dataPS.reduce(chain));
 })();
 
 export const dataProvider: Promise<DataProvider> = fakeDataDataProvider.then(
-  fakeDp =>
-    thisEndDataProvider.then(thisEndDp => {
-      return dp(fakeDp)
-        .chain(dp(thisEndDp))
-        .chain(backEndDataProvider)
-        .value();
+  (fakeDp) =>
+    thisEndDataProvider.then((thisEndDp) => {
+      return dp(fakeDp).chain(dp(thisEndDp)).chain(backEndDataProvider).value();
     })
 );
--- src/data/FakeData.ts
+++ src/data/FakeData.ts
@@ -2,19 +2,20 @@ import {
   chain,
   DataProvider,
   forResource,
-  withStaticData
+  withStaticData,
 } from "@quick-qui/data-provider";
 import {
   parseRef,
   REF_RESOLVE,
-  withExchangeModel
+  withExchangeModel,
 } from "@quick-qui/model-defines";
 import assert from "assert";
 import * as yaml from "js-yaml";
 import _ from "lodash";
 import { model } from "../Model";
 import { resolve, resolveAsString } from "../Resolve";
-
+import { env } from "../Env";
+import p from "path";
 //TODO fake data provider 是一个implementation， 应该以什么方式跟front、app-server 连接？
 
 export const fakeDataDataProvider: Promise<
@@ -22,11 +23,11 @@ export const fakeDataDataProvider: Promise<
 > = (async () => {
   const exchangeModel = withExchangeModel(await model)?.exchangeModel;
   const exchanges =
-    exchangeModel?.exchanges?.filter(exchange => {
+    exchangeModel?.exchanges?.filter((exchange) => {
       return exchange.to === "back" && exchange.from === "fake";
     }) ?? [];
   if (_.isEmpty(exchanges)) return undefined;
-  const providers = exchanges.map(async exchange => {
+  const providers = exchanges.map(async (exchange) => {
     //TODO 支持faker，或者唯一支持faker
     //TODO  目前的实现方法，每次改了fake以后app-server需要重启，不太理想，设计到两个环节，一个是model要重取，一个是fake 的dp要支持动态。
     let fakeData = exchange.parameters?.["fakeData"];
@@ -38,8 +39,13 @@ export const fakeDataDataProvider: Promise<
           REF_RESOLVE,
           `only support resolve, but got "${protocol}"`
         );
-        const base =
-          exchange.annotations?.["buildingContext"]?.modelFile?.repositoryBase;
+        //FIXME 有极大的问题，building context是model-server set的，是model-server容器内的path，不一定是app-server容器内的path。
+
+        const modelFile = exchange.annotations?.["buildingContext"]?.modelFile;
+
+        const base = modelFile
+          ? p.resolve(env.extendPath, modelFile.relativeToModelDir)
+          : env.extendPath;
         const dataString = await resolveAsString(path, base);
         if (path.endsWith(".json")) {
           fakeData = JSON.parse(dataString);
@@ -55,5 +61,5 @@ export const fakeDataDataProvider: Promise<
     return forResource(exchange.resources, dataProvider);
   });
 
-  return Promise.all(providers).then(dataPS => dataPS.reduce(chain));
+  return Promise.all(providers).then((dataPS) => dataPS.reduce(chain));
 })();
