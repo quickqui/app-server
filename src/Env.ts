fba86a298728f98acf542c48d7c2e8404bd30727
nielinjie <nielinjie@gmail.com>
2020-05-16 14:43:34 +0800

    debug

--- src/Env.ts
+++ src/Env.ts
@@ -9,12 +9,13 @@ export const env: {
   defaults = {
     modelUrl: "http://localhost:1111"
   };
+  console.log(JSON.stringify(process.env,undefined,2))
   return _.assign(
     {},
     defaults,
     filterObject({
       modelUrl: process.env.MODEL_URL,
-      extendPath: (process.env.EXTEND_PATH || process.env.MODEL_PATH)?? no("EXTEND_PATH")
+      extendPath: (process.env.EXTEND_PATH || process.env.MODEL_PATH)?? no("MODEL_PATH")
     })
   );
 })();
